ad8724181ca18add80ce392eaa62e9d809e3efd0
bithighlander <bithighlander@gmail.com>
2023-09-30 21:20:08 -0500

    bump

--- src/lib/layout/Meta.tsx
+++ src/lib/layout/Meta.tsx
@@ -4,21 +4,23 @@ const APP_NAME = "Swapkit Template";
 
 const Meta = () => {
   return (
-    <Helmet>
-      <title>{APP_NAME}</title>
-      <meta name="description" content="Pioneer Template" />
+    <div>
+      {/*<Helmet>*/}
+      {/*  <title>{APP_NAME}</title>*/}
+      {/*  <meta name="description" content="Pioneer Template" />*/}
 
-      <meta name="application-name" content={APP_NAME} />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="theme-color" content="#228B22" />
+      {/*  <meta name="application-name" content={APP_NAME} />*/}
+      {/*  <meta name="apple-mobile-web-app-capable" content="yes" />*/}
+      {/*  <meta name="apple-mobile-web-app-status-bar-style" content="default" />*/}
+      {/*  <meta name="apple-mobile-web-app-title" content={APP_NAME} />*/}
+      {/*  <meta name="format-detection" content="telephone=no" />*/}
+      {/*  <meta name="mobile-web-app-capable" content="yes" />*/}
+      {/*  <meta name="theme-color" content="#228B22" />*/}
 
-      <link rel="shortcut icon" href="/assets/favicon.svg" />
-      <link rel="manifest" href="/manifest.json" />
-    </Helmet>
+      {/*  <link rel="shortcut icon" href="/assets/favicon.svg" />*/}
+      {/*  <link rel="manifest" href="/manifest.json" />*/}
+      {/*</Helmet>*/}
+    </div>
   );
 };
 
